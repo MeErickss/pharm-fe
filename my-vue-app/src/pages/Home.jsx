ba5b63ce2b0a39c36a4aef657e027262c19289ad
fatia1 <130059555+MeErickss@users.noreply.github.com>
2025-01-25 01:10:03 -0300

    images

--- my-vue-app/src/pages/Home.jsx
+++ my-vue-app/src/pages/Home.jsx
@@ -3,6 +3,7 @@ export function Home(){
     return(
       <div className="flex-1 p-4 bg-gray-100">
         <h1 className="text-3xl font-bold mb-4">Home</h1>
+        <img src="../assets/g.png" alt="" />
       </div>
     )
 }
